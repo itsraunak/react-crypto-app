2e4724c47c3db238374f791d62c4438daf4d100c
Raunak Kumar Sah <itisraunak@gmail.com>
2023-10-14 12:30:01 +0530

    Added pagination to the coins page
    Signed-off-by: Raunak Kumar Sah <itisraunak@gmail.com>

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -20,7 +20,7 @@ const Coins = () => {
         setLoading(true);
     };
 
-    const btns = new Array(132).fill(1);
+    const btns = new Array(105).fill(1);
 
     useEffect(() => {
         const fetchCoins = async () => {
